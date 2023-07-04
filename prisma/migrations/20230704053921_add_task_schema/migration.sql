418ca9edd49c5d9173664745632791d84356fbd3
Mikhail Kraevskii <kramihm@gmail.com>
2023-07-04 15:03:04 +0700

    Migration for Task

---
+++ prisma/migrations/20230704053921_add_task_schema/migration.sql
@@ -0,0 +1,39 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "task";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "user";
+
+-- CreateEnum
+CREATE TYPE "task"."TaskStatus" AS ENUM ('CREATED', 'COMLETED');
+
+-- CreateTable
+CREATE TABLE "user"."User" (
+    "id" TEXT NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password_hash" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "task"."Task" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "status" "task"."TaskStatus" NOT NULL DEFAULT 'CREATED',
+    "order" DOUBLE PRECISION NOT NULL,
+    "taskListId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "task"."TaskList" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "TaskList_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "task"."Task" ADD CONSTRAINT "Task_taskListId_fkey" FOREIGN KEY ("taskListId") REFERENCES "task"."TaskList"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
